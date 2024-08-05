2ab44e80a5ca19baa83ae86565e590de31f5b784
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-04 19:30:45 -0500

    added main function

--- monty.h
+++ monty.h
@@ -33,6 +33,7 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+int main (int argc, char *argv[]);
 void m_push(stack_t **stack, int n);
 void m_pall(stack_t **stack, unsigned int line_number);
 void m_nop(stack_t **stack, unsigned int line_number);
